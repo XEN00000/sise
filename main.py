68ecffed9f0e46218649bf0e060e4f9a82050b0e
251592 Murza Łukasz <111185121+XEN00000@users.noreply.github.com>
2025-03-16 21:21:30 +0100

    BFS first steps of implementation
    begin of BFS

--- main.py
+++ main.py
@@ -1,66 +1,72 @@
-#
-#
-corePuzzle = [[1, 2, 3, 4],
-              [5, 6, 7, 8],
-              [9, 0, 11, 12],
-              [13, 10, 14, 15]]
-
-maxDepthLevel = 20
-maxMoves = 5
+goal_puzzle = [[1, 2, 3, 4],
+               [5, 6, 7, 8],
+               [9, 10, 11, 12],
+               [13, 14, 15, 0]]
 
 
-def swapField(row, col, puzzle):
-    if (row < 0 or row > 3) and (col < 0 or col > 3):
-        return
+def get_possible_moves(puzzle):
+    row, col = find_zero(puzzle)
 
-    zeroPosition = findBlank(puzzle)
-    swappingPosition = row, col
+    possible_moves = {}
 
-    x = puzzle[zeroPosition]
-    puzzle[zeroPosition] = puzzle[swappingPosition]
-    puzzle[swappingPosition] = x
+    if row > 0:
+        possible_moves["U"] = (row - 1, col)  # Can move up?
+    if row < 3:
+        possible_moves["D"] = (row + 1, col)  # Can move down?
+    if col > 0:
+        possible_moves["L"] = (row, col - 1)  # Can move left?
+    if col < 3:
+        possible_moves["R"] = (row, col + 1)  # Can move right?
+
+    print(list(possible_moves.keys()))
+    return possible_moves
+
+
+def do_the_move(moveSymbol, puzzle):
+    row, col = find_zero(puzzle)
+
+    if moveSymbol == "U":
+        puzzle[row][col], puzzle[row - 1][col] = puzzle[row - 1][col], puzzle[row][col]
+    if moveSymbol == "D":
+        puzzle[row][col], puzzle[row + 1][col] = puzzle[row + 1][col], puzzle[row][col]
+    if moveSymbol == "L":
+        puzzle[row][col], puzzle[row][col - 1] = puzzle[row][col - 1], puzzle[row][col]
+    if moveSymbol == "R":
+        puzzle[row][col], puzzle[row][col + 1] = puzzle[row][col + 1], puzzle[row][col]
+
     return puzzle
 
 
-def findBlank(puzzle):
-    for row in puzzle:
-        for col in row:
-            if puzzle[row][col] == 0:
-                return row, col
+def find_zero(puzzle):
+    x, y = 0, 0
+    while x <= 3:
+        while y <= 3:
+            if puzzle[x][y] == 0:
+                return x, y
+            y += 1
+        y = 0
+        x += 1
 
 
-def makeMove(puzzle, move):
-    move = move.lower()
-    row, col = findBlank(puzzle)
-    match move:
-        case 'r':
-            swappedPuzzle = swapField(row + 1, col, puzzle)
-            if swappedPuzzle == puzzle:
-                print("Cant make move")
-                return
-            else:
-                return swappedPuzzle
+def bfs(puzzle):
+    if puzzle == goal_puzzle:
+        return puzzle
 
-        case 'l':
-            swappedPuzzle = swapField(row - 1, col, puzzle)
-            if swappedPuzzle == puzzle:
-                print("Cant make move")
-                return
-            else:
-                return swappedPuzzle
+    nest_level = 0
+    tab_of_levels = list()
+    tab_of_levels.append(puzzle)
+    tab_of_levels.append(puzzle)
+    print(tab_of_levels)
 
-        case 'u':
-            swappedPuzzle = swapField(row, col - 1, puzzle)
-            if swappedPuzzle == puzzle:
-                print("Cant make move")
-                return
-            else:
-                return swappedPuzzle
+    while puzzle != goal_puzzle and nest_level < 5:
+        nest_level += 1
+        actual_level = list()
 
-        case 'd':
-            swappedPuzzle = swapField(row, col + 1, puzzle)
-            if swappedPuzzle == puzzle:
-                print("Cant make move")
-                return
-            else:
-                return swappedPuzzle
+
+if __name__ == '__main__':
+    shuffled_puzzle = [[1, 2, 3, 4],
+                       [5, 6, 7, 8],
+                       [9, 10, 11, 12],
+                       [13, 14, 0, 15]]
+
+    bfs(shuffled_puzzle)
