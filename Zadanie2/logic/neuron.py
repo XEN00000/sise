bd000d8bd9c36ea007ea121cc25a0ce87dca2db2
XEN00000 <111185121+XEN00000@users.noreply.github.com>
2025-04-18 23:43:41 +0200

    First sketch of neuron
    +bmi_dataset.csv to przykładowy plik na którym będziemy uczyć nasz neuron gdy zostanie już on w pełni zaimplementowany
    + first methods in neuron.py

--- Zadanie2/logic/neuron.py
+++ Zadanie2/logic/neuron.py
@@ -1,3 +1,37 @@
-class Neuron:
-    pass
+import numpy as np
 
+
+class Neuron:
+    def __init__(self, input_number):
+        """
+        W constructor mają być inicjalizowane wszystkie parametry neuronu takie jak:
+        - liczba jego wejść
+        - wagi wejść (można by je trzemać razem z wejściami. np. oznaczone numerami zamknięte w dict)
+        - czy został użyty bias
+        - (jeśli tak to jego wartość)
+        - szybkość uczenia się
+        """
+        self.weights = {i: np.random.uniform(-1, 1) for i in range(input_number)}
+        pass
+
+    def take_values(self):
+        """
+        W tej metodzie neuron będzie przyjmował wartości z wejść
+        Co więcej musi wywoływać metodę calculate_insput() aby odrazu było to przeliczane
+        """
+        pass
+
+    def calculate_output(self):
+        """
+        Tutaj mnożone będą wartośći przekazywane do neuronu przez wagi każdego z wejść.
+        Natępnie będą sumowane i dodawany będzie do nich bias
+        """
+        pass
+
+    def get_output(self):
+        """
+        Ta funkcja będzie zwracała wynik obliczony przez neuron
+        """
+
+    def set_weights(self, new_weights):
+        self.weights = new_weights
--- Zadanie2/view/main.py
+++ Zadanie2/view/main.py
@@ -1,2 +1,5 @@
+from Zadanie2.logic.neuron import Neuron
+
 if __name__ == "__main__":
-    print("Hello World!")
+    #print("Hello World!")
+    neuron = Neuron(1, 2, )
